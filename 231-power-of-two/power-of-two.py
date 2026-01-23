01b2fe03e0950614329de95b0746bb2a35ea4534
Rahul Gupta <rahulgupta193246@gmail.com>
2026-01-24 00:06:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (15.48%) - LeetSync

---
+++ 231-power-of-two/power-of-two.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n<=0:
+            return False
+       
+        if n%2!=0 and n!=1:
+            return False
+        temp=n
+        count=0
+        while temp%2==0:
+            temp=temp//2
+            count+=1
+        return temp==1
+
+
+        
