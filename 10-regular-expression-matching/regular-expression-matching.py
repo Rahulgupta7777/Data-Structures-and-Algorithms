2627199a4d94003aab6466079b78a0a01f03f026
Rahul Gupta <rahulgupta193246@gmail.com>
2026-02-07 13:39:49 +0530

    Time: 15 ms (59.16%) | Memory: 13 MB (5.69%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -1,29 +1,30 @@
 class Solution(object):
     def isMatch(self, s, p):
-        m, n = len(s), len(p)
-        
-        dp = [[False] * (n + 1) for _ in range(m + 1)]
-        dp[0][0] = True
-        
+        memo = {}
 
-        for j in range(2, n + 1):
-            if p[j - 1] == '*':
-                dp[0][j] = dp[0][j - 2]
-        
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                
+        def dp(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
 
-                if p[j - 1] == s[i - 1] or p[j - 1] == '.':
-                    dp[i][j] = dp[i - 1][j - 1]
-                
 
-                elif p[j - 1] == '*':
+            if j == len(p):
+                return i == len(s)
 
-                    dp[i][j] = dp[i][j - 2]
-                    
+            first_match = (
+                i < len(s) and
+                (p[j] == s[i] or p[j] == '.')
+            )
 
-                    if p[j - 2] == s[i - 1] or p[j - 2] == '.':
-                        dp[i][j] |= dp[i - 1][j]
-        
-        return dp[m][n]
+
+            if j + 1 < len(p) and p[j + 1] == '*':
+                ans = (
+                    dp(i, j + 2) or        
+                    (first_match and dp(i + 1, j))  
+                )
+            else:
+                ans = first_match and dp(i + 1, j + 1)
+
+            memo[(i, j)] = ans
+            return ans
+
+        return dp(0, 0)
